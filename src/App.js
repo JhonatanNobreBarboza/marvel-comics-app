89d2012b5cf7eb4c0d6b7cb0a014f97917c2fcc1
jhonatan <jhonatan.souza@novaandradina.org>
2019-05-12 18:43:56 -0400

    estruturação pagina principal

--- src/App.js
+++ src/App.js
@@ -18,9 +18,113 @@ import {
   IconButton
 } from "@material-ui/core";
 
+const styles = () => ({
+  root: {
+    padding: "50px 100px",
+    zIndex: 999,
+    position: "absolute"
+  },
+  card: {
+    display: "flex",
+    height: "calc(100vh - 100px)"
+  },
+  rightBorder: {
+    borderRight: "solid #d0D0D0 1px"
+  },
+  content: {
+    marginTop: 0
+  },
+  background: {
+    position: "absolute",
+    height: 200,
+    width: "100%",
+    top: 0,
+    background: "#7159C1"
+  },
+  rightContainer: {
+    background:
+      "url(https://o.aolcdn.com/images/dims?quality=85&image_uri=https%3A%2F%2Fo.aolcdn.com%2Fimages%2Fdims%3Fcrop%3D1200%252C760%252C0%252C0%26quality%3D85%26format%3Djpg%26resize%3D1600%252C1015%26image_uri%3Dhttps%253A%252F%252Fs.yimg.com%252Fos%252Fcreatr-uploaded-images%252F2019-04%252Fe44224e0-6024-11e9-bfef-da4be9aaa31b%26client%3Da1acac3e1b3290917d92%26signature%3De8479021fa5effe690339b9a2c291735c12343f4&client=amp-blogside-v2&signature=1e1df199acdf8e543a27d36079c12461796b4e4c) center center",
+    flex: 1
+  },
+  heightAdjust: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  paper: {
+    background: "#9de1fe",
+    padding: 20
+  },
+  information: {
+    color: "#444"
+  }
+});
+
 
 const App = ({ classes }) => (
+  <div>
+    <div className={classes.background} />
+    <Grid container className={classes.root}>
+      <Grid item xs={12}>
+        <Card className={classes.card}>
+          <Grid container>
+            <LeftContainer classes={classes} />
+            <RightContainer classes={classes} />
+          </Grid>
+        </Card>
+      </Grid>
+    </Grid>
+  </div>
 );
 
+const list = [
+  { id: 1, name: "Capitão América", text: "Lorem ipsum", image: <ImageIcon /> },
+  { id: 2, name: "Homem Aranha", text: "Lorem ipsum", image: <WorkIcon /> },
+  { id: 3, name: "Thor", text: "Lorem ipsum", image: <BeachAccessIcon /> }
+];
+
+const LeftContainer = ({ classes }) => (
+  <Grid item xs={3}>
+    <CardHeader
+      className={classes.rightBorder}
+      avatar={
+        <Avatar aria-label="Recipe" className={classes.avatar}>
+          H
+        </Avatar>
+      }
+    />
+    <Paper className={classes.paper} elevation={0}>
+      <Typography className={classes.information} variant="subheader">
+        Conhecendo seus Super Heróes favoritos!
+      </Typography>
+    </Paper>
+    <List>
+      {list.map(item => (
+        <ListItem>
+          <Avatar>{item.image}</Avatar>
+          <ListItemText primary={item.name} secondary={item.text} />
+        </ListItem>
+      ))}
+    </List>
+  </Grid>
+);
+
+const RightContainer = ({ classes }) => (
+  <Grid className={classes.heightAdjust} item xs={9}>
+    <CardHeader
+      avatar={
+        <Avatar aria-label="Recipe" className={classes.avatar}>
+          <ImageIcon />
+        </Avatar>
+      }
+      action={
+        <IconButton>
+          <MoreVertIcon />
+        </IconButton>
+      }
+      title="Marvel Comics"
+    />
+    <CardContent className={[classes.rightContainer, classes.content]} />
+  </Grid>
+);
 
 export default withStyles(styles)(App);
